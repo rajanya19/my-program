68d7e857acb143a3b2d1c453226e3b58d2ad90f6
rajanya19 <rajanyaghoshmalda19@gmail.com>
2026-02-20 23:20:29 +0530

    Add files via upload

---
+++ c controlstatement 2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	int a,b,c;
+	printf("enter three numbers:");
+	scanf("%d %d %d",&a,&b,&c);
+	if(a>b){
+		if(a>c)
+		printf("%d is larger",a);
+	}else if (c>a){
+		printf("%d is larger",c);
+	}else if (b>a){
+		printf("%d is larger",b);
+	}
+	else{
+		printf("invalid");
+	}
+	return 0;
+}
